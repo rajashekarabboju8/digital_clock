d110fcf2229ccc0de774c717d19c8679b7e4c7d4
raja <raja@gmail.com>
2022-12-14 11:44:59 +0530

    updating the clock to redscreen version

--- digitalclock.py
+++ digitalclock.py
@@ -1,7 +1,19 @@
 from tkinter import Tk, Label
+from datetime import datetime
 
 window = Tk()
 window.title("Digital Clock")
 window.geometry("600x300")
+window.configure(bg="red")
+
+label = Label(window,font=("Arial Black",78,"bold"),bg="red",fg="white")
+label.pack(pady=100)
+
+def clock():
+    time = datetime.now().strftime("%H:%M:%S")
+    label.configure(text=time)
+    label.after(500,clock)
+
+clock()    
 
 window.mainloop()
